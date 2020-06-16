247c5b73cd7fbbf04f1f12af32ebaa534114ba6c
mczefirka <ganisevskij@gmail.com>
2020-06-16 15:14:38 +0300

    Lab 2 task 1 (2)

---
+++ CS Labs/Lab 2/Program_2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_1;
+
+namespace Lab_2
+{
+    class Program_2
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Choose program:\n" +
+                                "1 — Task 1 (2)\n" +
+                                "2 — Task 2 ( )\n" +
+                                "3 — Task 3 ( )\n" +
+                                "0 — Exit");
+
+                int MenuItem = int.Parse(Console.ReadLine());
+                Class_Task1 class_t1 = new Class_Task1();
+                switch (MenuItem)
+                {
+                    case 1:
+                        class_t1.Task1_2();
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
--- CS Labs/Lab 2/Task 1 (2).cs
+++ CS Labs/Lab 2/Task 1 (2).cs
@@ -4,9 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab_2
+namespace Task_1
 {
-    class Class1
+    public class Class_Task1
     {
+        public void Task1_2()
+        {
+            try
+            {
+                int Ctr;
+                do
+                {
+                    Console.WriteLine("\nEnter some words");
+
+                    string sentence = Console.ReadLine();
+                    string[] words = sentence.Split(' ');//расщепляет на слова
+                    Console.WriteLine("\nreversed:\n");
+                    Array.Reverse(words);
+
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        Console.Write(words[i] + " ");
+                    }
+
+                    Console.WriteLine("\n\nExit?\n" +
+                        "1 — yes\n" +
+                        "0 — no");
+                    Ctr = int.Parse(Console.ReadLine());
+
+                } while (Ctr == 0);
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
     }
 }
