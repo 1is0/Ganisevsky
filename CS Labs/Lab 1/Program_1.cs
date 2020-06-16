64c36521be2dac99f58e6aa4539a03a6290102dc
mczefirka <ganisevskij@gmail.com>
2020-06-16 14:28:41 +0300

    Lab 1 C#

---
+++ CS Labs/Lab 1/Program_1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Forms;
+using Write_Text;
+
+namespace ConsoleApp1
+{
+    class Program_1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int FirstValue;
+                Class_Write_Text write_Text = new Class_Write_Text();
+
+                Console.WriteLine("Добро пожаловать в текстовый квест." +
+                    "\nВы решили поработать за компьютером, но после нажатия кнопки питания он не включается. " +
+                    "Что же делать?");
+
+                do
+                {
+                    Console.WriteLine("\n\n1\t— Может я снова забыл его подключить?\n\tПроверить подключен ли ПК к электросети\n" +
+                        "\n2\t— Что он опять натворил с компьютером?\n\tПопросить помощи у сына\n" +
+                        "\n3\t— Попробую разобраться сам\n\tОткрыть корпус ПК и разобраться в чём дело\n" +
+                        "\n0\tВыход из программы");
+                    FirstValue = int.Parse(Console.ReadLine());
+
+                    switch (FirstValue)
+                    {
+                        case 1:
+                            write_Text.Text_1();
+                            break;
+                        case 2:
+                            write_Text.Text_2();
+                            break;
+                        case 3:
+                            write_Text.Text_3();
+                            break;
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+                    }
+
+                } while (FirstValue != 0);
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка. Неверный формат ввода");
+            }
+        }
+    }
+}
---
+++ CS Labs/Lab 1/Text.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Write_Text
+{
+    public class Class_Write_Text
+    {
+        public void Text_1()
+        {
+            Console.WriteLine("\nКомпьютер подключен к сети. Нужно искать решение дальше");
+        }
+
+        public void Text_2()
+        {
+            Console.WriteLine("\nК сожалению сын не может помочь: он пошёл играть в футбол");
+        }
+        public void Text_3()
+        {
+            try
+            {
+                int FirstValue;
+
+                Console.WriteLine("\nОткрыв крышку вы вспомнили, что недавно сын менял видеокарту и заодно почистил компьютер");
+
+                do
+                {
+                    Console.WriteLine("\n1\tПроверить подключение видеокарты" +
+                        "\n2\tПроверить подключение комплектующих к блоку питания" +
+                        "\n0\tНазад");
+                    FirstValue = int.Parse(Console.ReadLine());
+
+                    switch (FirstValue)
+                    {
+                        case 1:
+                            Console.WriteLine("\nВидеокарта подключена верно. И дело не в ней, так как ПК вообще не включается");
+                            break;
+                        case 2:
+                            Console.WriteLine("\nМатеринская плата была плохо подключена к болоку питания. " +
+                                "Вы переподключили кабель, теперь можно работать");
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+                    }
+
+                } while (FirstValue != 0);
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка. Неверный формат ввода");
+            }
+        }
+    }
+}
